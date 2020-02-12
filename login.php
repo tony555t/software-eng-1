f0e070ca6622719a68c3d01a7eac00d4a0d16791
tony555t <60977103+tony555t@users.noreply.github.com>
2020-02-12 18:45:45 +0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>My first PHP Website</title>
+</head>
+<body>
+	<h2>Login Page</h2>
+	<a href="index.php">Click here to go back</a><br><br/>
+	<form action="checklogin.php"method="post">
+		Enter Username:<input type="text" name="username"required="required"/><br/>
+		Enter Password:<input type="password" name="password"required="required"/><br/>
+		<input type="submit"value="Login"/>
+	</form>
+</body>
+</html>
