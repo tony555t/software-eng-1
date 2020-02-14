5b6245aabea40d257498760a7077cf602280fcff
tony555t <60977103+tony555t@users.noreply.github.com>
2020-02-14 11:39:10 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>My first PHP Website</title>
+</head>
+<body>
+	<?php
+	echo"<p>Hello World!</p>";
+	?>
+	<a href="login.php">Click here to login</a><br/>
+	<a href="register.php">Click here to register</a>
+</body>
+</html>
